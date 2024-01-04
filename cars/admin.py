38f66eab8a65f10273e3bfef62ca8a6f1217e91b
Sai Krishna Reddy Gaddam <gaddam.sai@northeastern.edu>
2024-01-04 15:43:28 -0500

    Added Car model to admin panel

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from cars.models import Car
+
+# Register your models here
+
+# django 1.x to 2.x syntax
+
+# no customization syntax(simple syntax)
+# admin.site.register(Car)
+
+#customization syntax
+# class CarAdmin(admin.ModelAdmin):
+#     # fields = ["year", "brand"] #to order the fields
+#     #to order the field sets
+#     fieldsets = [
+#         ("Date Information", {
+#             "fields": ["year"]
+#         }),
+#         ("Car Information", {
+#             "fields": ["brand"]
+#         })
+#     ]
+
+# admin.site.register(Car, CarAdmin)
+
+#django 2.x or later syntax(but any syntax is fine)
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    # write pass here if don't want want any customizations
+    # fields = ["year", "brand"] #to order the fields
+    #to order the field sets
+    fieldsets = [
+        ("Date Information", {
+            "fields": ["year"]
+        }),
+        ("Car Information", {
+            "fields": ["brand"]
+        })
+    ]
