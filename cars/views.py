bccd15951e2732cfa9b78cfb45a93cb7e98d1196
Sai Krishna Reddy Gaddam <gaddam.sai@northeastern.edu>
2024-01-04 11:58:44 -0500

    added functionality to list, add and delete cars

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from . import models
 # Create your views here.
 
 def list_view(request):
-    return render(request, "cars/list.html")
+    cars = models.Car.objects.all()
+    return render(request, "cars/list.html", context={"cars": cars})
 
 
 def add_view(request):
-    return render(request, "cars/add.html")
+    if request.POST:
+        brand = request.POST["brand"]
+        year = int(request.POST["year"])
+        models.Car.objects.create(brand=brand, year=year)
+        return redirect(reverse("cars:list-cars"))
+    else:
+        return render(request, "cars/add.html")
 
 def delete_view(request):
-    return render(request, "cars/delete.html")
+    if request.POST:
+        pk = request.POST["pk"]
+        try:
+            models.Car.objects.get(pk=pk).delete()
+        except:
+            print("PK not found")
+        return redirect(reverse("cars:list-cars"))
+    else:
+        return render(request, "cars/delete.html")
